bf11f7394efb93f766b2c62988313365d90a4ca0
Thirumal <m.thirumal@hotmail.com>
2019-12-26 20:08:07 +0530

    first commit

---
+++ src/main/java/in/co/thirumal/CustomMethodSecurityApplication.java
@@ -0,0 +1,13 @@
+package in.co.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomMethodSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomMethodSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/co/thirumal/CustomMethodSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.thirumal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomMethodSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
