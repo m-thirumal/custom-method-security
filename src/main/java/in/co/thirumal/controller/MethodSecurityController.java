e31d8577878d5e342ca581dafde6fc846762a3e3
Thirumal <m.thirumal@hotmail.com>
2019-12-26 22:11:27 +0530

    me

---
+++ src/main/java/in/co/thirumal/controller/MethodSecurityController.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package in.co.thirumal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author thirumal
+ *
+ */
+@RestController
+@RequestMapping("method")
+public class MethodSecurityController {
+
+	
+}
