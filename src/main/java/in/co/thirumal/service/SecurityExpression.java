b6bae5fc664349a6cfc96fad6ff0d0197b00d4c1
Thirumal <m.thirumal@hotmail.com>
2019-12-27 19:00:35 +0530

    security

---
+++ src/main/java/in/co/thirumal/service/SecurityExpression.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package in.co.thirumal.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author thirumal
+ *
+ */
+@Service
+public class SecurityExpression {
+
+}
