4097c1e0a6fcc281d47c66b48582da5550610087
Thirumal <m.thirumal@hotmail.com>
2019-12-30 20:25:32 +0530

    Request param

--- src/main/java/in/co/thirumal/controller/MethodSecurityController.java
+++ src/main/java/in/co/thirumal/controller/MethodSecurityController.java
@@ -5,8 +5,8 @@ package in.co.thirumal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.co.thirumal.service.MethodSecurityService;
@@ -22,8 +22,8 @@ public class MethodSecurityController {
 	@Autowired
 	MethodSecurityService methodSecurityService;
 	
-	@GetMapping(value = "/{name}")
-	public String sayHello(@PathVariable("name") String name) {
+	@GetMapping(value = "")
+	public String sayHello(@RequestParam(value = "name", defaultValue = "thirumal") String name) {
 		return methodSecurityService.sayHello(name);
 	}
 	
